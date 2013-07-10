25f2b53922e781e0d5644f444bbdd196338cb661
Raphael Fabeni <rapha.fabeni@gmail.com>
2013-07-10 10:54:25 -0300

    Teste jekyll

--- README.md
+++
@@ -1,4 +0,0 @@
-Guia de Estilos - Educação SP
-==================
-
-Guia de estilos para os projetos das coordenadorias e secretarias do portal da Educação de SP.
--- files/demo/in/header.php
+++
@@ -1,3 +0,0 @@
-<header class="header" role="banner">
-   <h1><span class="pull-left">Guia de Estilos</span> <span class="pull-right">Educação SP</span></h1>   
-</header>
